c0a618f33c47240e8cb7229a4a3cca416d0ad732
shyshenok <shyshenoka@gmail.com>
2018-03-06 15:20:02 +0200

    Init repository

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:planets_flutter/ui/home/HomePage.dart';
+
+void main() {
+//  Routes.initRoutes();
+  runApp(new MaterialApp(
+    title: "Planets",
+    home: new HomePage(),
+  ));
+}
+
---
+++ lib/ui/home/HomePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:planets_flutter/ui/home/GradientAppBar.dart';
+
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Column(
+        children: <Widget>[
+          new GradientAppBar("treva"),
+        ],
+      ),
+    );
+  }
+}
