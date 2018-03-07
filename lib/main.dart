fb798e140e571a12b69e2588ea755cc30d6fde35
shyshenok <shyshenoka@gmail.com>
2018-03-07 13:24:49 +0200

    Create list of planets

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:planets_flutter/ui/home/HomePage.dart';
 
 void main() {
-//  Routes.initRoutes();
   runApp(
       new MaterialApp(
         title: "Planets",
